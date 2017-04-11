700ca7a0ae1a92ef67c1618d8ac75676b5899790
helpgramteam <amir1ali2robat@gmail.com>
2017-04-11 19:44:07 +0430

    amir

--- helpgram.sh
+++ helpgram.sh
@@ -4,15 +4,17 @@ cd $HOME/classicbot
 
 install() {
 	    cd tg
-		sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-		sudo apt-get install g++-4.7 c++-4.7
-		sudo apt-get update		
-        sudo apt-get upgrade
-		sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-		sudo apt-get install lua-lgi
-		sudo apt-get install libnotify-dev
-		sudo apt-get install screen
-		sudo apt-get install tmux
+		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 -y c++-4.7 -y
+		sudo apt-get update
+		sudo apt-get upgrade
+		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+		sudo apt-get install screen -y
+		sudo apt-get install tmux -y
+		sudo apt-get install libstdc++6 -y
+		sudo apt-get install lua-lgi -y
+		sudo apt-get install libnotify-dev -y
+		sudo service redis-server restart
 		wget https://valtman.name/files/telegram-cli-1222
 		mv telegram-cli-1222 tgcli
 		chmod +x tgcli
@@ -36,4 +38,5 @@ fi
    echo -e "\e[36m"
    ./tg/tgcli -s ./bot/bot.lua $@
 fi
+
 
